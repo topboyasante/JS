0e9e6b6f593dfd6f6adf8646552505c69f77fa02
Nana Kwasi <topboyasante@gmail.com>
2022-02-02 12:08:08 +0000

    commit

--- basics.js
+++ basics.js
@@ -15,3 +15,15 @@ console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
 let Newton = celsius*(33/100);
 Newton = Math.floor(celsius*(33/100));
 console.log(`The temperature is ${Newton} degrees Newton.`)
+
+//Dog Age Calculation App:
+let myAge = 19; //variable for my age.
+var earlyYears = 2;//variable called early years.
+earlyYears *=10.5;
+let laterYears = myAge-2; //new variable laterYears = myage -2
+laterYears*=4;
+myAgeInDogYears = earlyYears + laterYears;
+myName='Kwasi Asante'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years
+whic is ${myAgeInDogYears} in Dog Years.`)
